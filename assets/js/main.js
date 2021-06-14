46114eaa7807e610931dc2f9250b0fd8cbdb294d
Your Name <you@example.com>
2021-06-14 19:50:48 +0700

    hi

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+	const navText = ["<i class='bx bx-chevron-left'></i>","<i class='bx bx-chevron-right'></i>" ];
+    $("#hero-slider").owlCarousel({
+    	items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        navText: navText,
+        autoplayHoverPause: true
+    })
+
+     $("#feedback-slider").owlCarousel({
+    	items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        navText: navText,
+        autoplayHoverPause: true
+    })
+
+    $("#tab-slide").owlCarousel({
+    	items: 3,
+        dots: false,
+        loop: true,
+        nav: true,
+        autoplay: false,
+        navText: navText,
+        autoplayHoverPause: true
+    })
+
+    // tab product
+ 	var tabs = $(".tab")
+ 	var panes = $(".pane")
+ 	tabs.click(function(){
+ 		tabs.removeClass("active")
+ 		$(this).addClass("active")
+ 		panes.hide()
+ 		var pane = $(this).attr("data-tab")
+ 		$(pane).fadeIn(300)
+ 	})
+ 	$("#tab-slide .tab-slide-item h3:first").click()
+});
